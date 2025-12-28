4c5e4c866d1b5c67481266a6b76869e8cfd973c8
tiennguyen261205-tech <tiennguyen261202@gmail.com>
2025-12-28 13:28:07 +0700

    Cap nhat phan order trong admin.js

--- js/admin.js
+++ js/admin.js
@@ -290,6 +290,156 @@ function getMoney() {
        });
    }
 
+   /* ==============================================
+   QUẢN LÝ ĐƠN HÀNG (ORDER)
+   ============================================== */
+function showOrder(arr) {
+    let orderHtml = "";
+    if (arr.length == 0) {
+        orderHtml = `<td colspan="6">Không có dữ liệu</td>`
+    } else {
+        arr.forEach((item) => {
+            let status = item.trangthai == 0 ? `<span class="status-no-complete">Chưa xử lý</span>` : `<span class="status-complete">Đã xử lý</span>`;
+            let date = formatDate(item.thoigiandat);
+            orderHtml += `
+            <tr>
+                <td>${item.id}</td>
+                <td>${item.khachhang}</td>
+                <td>${date}</td>
+                <td>${vnd(item.tongtien)}</td>                      
+                <td>${status}</td>
+                <td class="control">
+                    <button class="btn-detail" onclick="detailOrder('${item.id}')"><i class="fa-regular fa-eye"></i> Chi tiết</button>
+                </td>
+            </tr>`;
+        });
+    }
+    document.getElementById("showOrder").innerHTML = orderHtml;
+}
+
+function findOrder() {
+    let tinhTrang = parseInt(document.getElementById("tinh-trang").value);
+    let ct = document.getElementById("form-search-order").value;
+    let timeStart = document.getElementById("time-start").value;
+    let timeEnd = document.getElementById("time-end").value;
+
+    if (timeEnd < timeStart && timeEnd != "" && timeStart != "") {
+        alert("Lựa chọn thời gian sai !");
+        return;
+    }
+    let orders = localStorage.getItem("order") ? JSON.parse(localStorage.getItem("order")) : [];
+    let result = tinhTrang == 2 ? orders : orders.filter((item) => item.trangthai == tinhTrang);
+    
+    result = ct == "" ? result : result.filter((item) => {
+        return (item.khachhang.toLowerCase().includes(ct.toLowerCase()) || item.id.toString().toLowerCase().includes(ct.toLowerCase()));
+    });
+
+    if (timeStart != "" && timeEnd == "") {
+        result = result.filter((item) => new Date(item.thoigiandat) >= new Date(timeStart).setHours(0, 0, 0));
+    } else if (timeStart == "" && timeEnd != "") {
+        result = result.filter((item) => new Date(item.thoigiandat) <= new Date(timeEnd).setHours(23, 59, 59));
+    } else if (timeStart != "" && timeEnd != "") {
+        result = result.filter((item) => new Date(item.thoigiandat) >= new Date(timeStart).setHours(0, 0, 0) && new Date(item.thoigiandat) <= new Date(timeEnd).setHours(23, 59, 59));
+    }
+    showOrder(result);
+}
+
+function detailOrder(id) {
+    document.querySelector(".modal.detail-order").classList.add("open");
+    let orders = localStorage.getItem("order") ? JSON.parse(localStorage.getItem("order")) : [];
+    let products = localStorage.getItem("products") ? JSON.parse(localStorage.getItem("products")) : [];
+    let order = orders.find((item) => item.id == id);
+    let ctDon = getOrderDetails(id);
+    
+    let spHtml = `<div class="modal-detail-left"><div class="order-item-group">`;
+    ctDon.forEach((item) => {
+        let detaiSP = products.find(product => product.id == item.id);
+        // Fallback nếu sản phẩm đã bị xóa
+        let title = detaiSP ? detaiSP.title : "Sản phẩm đã xóa";
+        let img = detaiSP ? detaiSP.img : "./assets/img/blank-image.png";
+        
+        spHtml += `<div class="order-product">
+            <div class="order-product-left">
+                <img src="${img}" alt="">
+                <div class="order-product-info">
+                    <h4>${title}</h4>
+                    <p class="order-product-note"><i class="fa-light fa-pen"></i> ${item.note}</p>
+                    <p class="order-product-quantity">SL: ${item.soluong}<p>
+                </div>
+            </div>
+            <div class="order-product-right">
+                <div class="order-product-price">
+                    <span class="order-product-current-price">${vnd(item.price)}</span>
+                </div>
+            </div>
+        </div>`;
+    });
+    spHtml += `</div></div>`;
+    
+    spHtml += `<div class="modal-detail-right">
+        <ul class="detail-order-group">
+            <li class="detail-order-item">
+                <span class="detail-order-item-left"><i class="fa-light fa-calendar-days"></i> Ngày đặt hàng</span>
+                <span class="detail-order-item-right">${formatDate(order.thoigiandat)}</span>
+            </li>
+            <li class="detail-order-item">
+                <span class="detail-order-item-left"><i class="fa-light fa-truck"></i> Hình thức giao</span>
+                <span class="detail-order-item-right">${order.hinhthucgiao}</span>
+            </li>
+            <li class="detail-order-item">
+                <span class="detail-order-item-left"><i class="fa-thin fa-person"></i> Người nhận</span>
+                <span class="detail-order-item-right">${order.tenguoinhan}</span>
+            </li>
+            <li class="detail-order-item">
+                <span class="detail-order-item-left"><i class="fa-light fa-phone"></i> Số điện thoại</span>
+                <span class="detail-order-item-right">${order.sdtnhan}</span>
+            </li>
+            <li class="detail-order-item tb">
+                <span class="detail-order-item-left"><i class="fa-light fa-clock"></i> Thời gian giao</span>
+                <p class="detail-order-item-b">${(order.thoigiangiao == "" ? "" : (order.thoigiangiao + " - ")) + formatDate(order.ngaygiaohang)}</p>
+            </li>
+            <li class="detail-order-item tb">
+                <span class="detail-order-item-t"><i class="fa-light fa-location-dot"></i> Địa chỉ nhận</span>
+                <p class="detail-order-item-b">${order.diachinhan}</p>
+            </li>
+            <li class="detail-order-item tb">
+                <span class="detail-order-item-t"><i class="fa-light fa-note-sticky"></i> Ghi chú</span>
+                <p class="detail-order-item-b">${order.ghichu}</p>
+            </li>
+        </ul>
+    </div>`;
+    
+    document.querySelector(".modal-detail-order").innerHTML = spHtml;
+
+    let classDetailBtn = order.trangthai == 0 ? "btn-chuaxuly" : "btn-daxuly";
+    let textDetailBtn = order.trangthai == 0 ? "Chưa xử lý" : "Đã xử lý";
+    document.querySelector(".modal-detail-bottom").innerHTML = `<div class="modal-detail-bottom-left">
+        <div class="price-total">
+            <span class="thanhtien">Thành tiền</span>
+            <span class="price">${vnd(order.tongtien)}</span>
+        </div>
+    </div>
+    <div class="modal-detail-bottom-right">
+        <button class="modal-detail-btn ${classDetailBtn}" onclick="changeStatus('${order.id}',this)">${textDetailBtn}</button>
+    </div>`;
+}
+
+function getOrderDetails(madon) {
+    let orderDetails = localStorage.getItem("orderDetails") ? JSON.parse(localStorage.getItem("orderDetails")) : [];
+    return orderDetails.filter((item) => item.madon == madon);
+}
+
+function changeStatus(id, el) {
+    let orders = JSON.parse(localStorage.getItem("order"));
+    let order = orders.find((item) => item.id == id);
+    order.trangthai = 1;
+    el.classList.remove("btn-chuaxuly");
+    el.classList.add("btn-daxuly");
+    el.innerHTML = "Đã xử lý";
+    localStorage.setItem("order", JSON.stringify(orders));
+    findOrder(); // Refresh lại danh sách
+}
+
 function showThongKe(arr, mode) {
     // Merge sản phẩm giống nhau
     let mergeObj = [];
